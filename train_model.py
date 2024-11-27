be15eeed5dbf5098532f378609991fcdbdd75827
Hisan Usman <101946933+hisanusman@users.noreply.github.com>
2024-11-27 23:00:22 +0500

    Pushed modified train_model.py file

--- train_model.py
+++ train_model.py
@@ -1,24 +1,30 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+import numpy as np
 import pickle
 
 def train_model(input_file="processed_data.csv", model_file="model.pkl"):
-    # Load processed data
     data = pd.read_csv(input_file)
 
-    # Prepare features and target
-    X = data[["Humidity", "Wind Speed"]]
-    y = data["Temperature"]
+    X = data[["Humidity", "Wind Speed"]].values
+    y = data["Temperature"].values  
 
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    np.random.seed(42)
+    indices = np.random.permutation(len(X))
+    test_size = int(0.2 * len(X))
+    train_indices = indices[test_size:]
+    test_indices = indices[:test_size]
 
-    # Train linear regression model
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+    X_train, X_test = X[train_indices], X[test_indices]
+    y_train, y_test = y[train_indices], y[test_indices]
 
-    # Save the model
+    X_train_bias = np.c_[np.ones(X_train.shape[0]), X_train]
+    X_test_bias = np.c_[np.ones(X_test.shape[0]), X_test]
+
+    theta = np.linalg.inv(X_train_bias.T @ X_train_bias) @ X_train_bias.T @ y_train
+
+    y_pred = X_test_bias @ theta
+
+    model = {"coefficients": theta[1:], "intercept": theta[0]}
     with open(model_file, "wb") as file:
         pickle.dump(model, file)
 
